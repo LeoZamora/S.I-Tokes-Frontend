b4c075bb08d7b89119771bc6313a67df7135a9d9
LeoZamora <leozamora2003@outlook.com>
2025-06-01 19:36:25 -0600

    Proveedores y facturacion con avances significativos, trabajando en el modulo de inventario

---
+++ src/components/movimientos/Compras/dialogsCompras/ViewOrdenes.vue
@@ -0,0 +1,223 @@
+<template>
+    <v-dialog v-model="localShow" max-width="600" persistent>
+        <v-card id="diag-fact">
+            <v-card-title class="bg-red-darken-4 d-flex align-center">
+                <h5><v-icon>mdi-file-document-outline</v-icon>ÓRDEN</h5>
+                <v-spacer />
+                <v-btn icon size="small" color="white" variant="tonal" @click="closeDialog()">
+                    <v-icon>mdi-close</v-icon>
+                    <v-tooltip activator="parent" location="top" text="Cerrar" />
+                </v-btn>
+            </v-card-title>
+            <v-divider />
+            <v-card-text id="body-card" >
+                <v-card-subtitle class="d-flex align-center text-center mb-4">
+                    <small class="font-weight-bold">GENERALES</small>
+                    <v-divider/>
+                </v-card-subtitle>
+                <v-row>
+                    <v-col cols="12" md="12" sm="12">
+                        <div class="d-flex justify-space-between align-center mb-1">
+                            <small class="text-grey"> Nº orden:</small>
+                            <small><strong>{{ data.orden.numorden }}</strong></small>
+                        </div>
+                        <div class="d-flex justify-space-between align-center mb-1">
+                            <small class="text-grey"><strong>C$</strong> Córdobas:</small>
+                            <small><strong>{{ data.nio ? 'Si' : 'No' }}</strong></small>
+                        </div>
+                        <div class="d-flex justify-space-between align-center mb-1">
+                            <small class="text-grey">Dólares:</small>
+                            <small><strong>{{ data.usd ? 'Si' : 'No' }}</strong></small>
+                        </div>
+                        <div class="d-flex justify-space-between align-center mb-1">
+                            <small class="text-grey">Imptos:</small>
+                            <small><strong>{{ data.usd ? 'Si' : 'No' }}</strong></small>
+                        </div>
+                        <div class="d-flex justify-space-between align-center mb-1">
+                            <small class="text-grey">Fecha Registro:</small>
+                            <small><strong>{{ formateDate(data.orden.fechaRegistro) }}</strong></small>
+                        </div>
+                        <v-card-subtitle class="d-flex align-center text-center my-4">
+                            <v-divider/>
+                        </v-card-subtitle>
+                        <div class="d-flex justify-space-between align-center mb-1">
+                            <small class="text-grey">Proveedor:</small>
+                            <small><strong>{{ data.orden.proveedor }}</strong></small>
+                        </div>
+                        <div class="d-flex justify-space-between align-center mb-1">
+                            <small class="text-grey">Ruc Prov:</small>
+                            <small><strong>{{ data.orden.rucProveedor }}</strong></small>
+                        </div>
+                        <div class="d-flex justify-space-between align-center mb-1">
+                            <small class="text-grey">Factura Prov:</small>
+                            <small><strong>{{ data.orden.facturaProv }}</strong></small>
+                        </div>
+                        <div class="d-flex justify-space-between align-center mb-1">
+                            <small class="text-grey">Emp. Registro:</small>
+                            <small><strong>{{ data.orden.empleado }}</strong></small>
+                        </div>
+                    </v-col>
+                </v-row>
+                <v-card-subtitle class="d-flex align-center text-center my-4">                    
+                    <small class="font-weight-bold">DETALLES</small>
+                    <v-divider/>
+                </v-card-subtitle>
+                <v-row>
+                    <v-col cols="12" sm="12" md="12">
+                        <v-data-table class="border rounded font" density="compact" :headers="data.headers">
+                            <template v-slot:item.opc>
+                                <v-tooltip text="Eliminar" location="top">
+                                    <template v-slot:activator="{ props }">
+                                        <v-icon v-bind="props" color="error" class="mr-1">mdi-delete</v-icon>
+                                    </template>
+                                </v-tooltip>
+                            </template>
+                            <template v-slot:item.precio="{ item }">
+                                <div>{{ formatedCurrency(item.precio) }}</div>
+                            </template>
+                        </v-data-table>
+                    </v-col>
+                    <v-col cols="12" md="6" sm="6">
+                        <v-textarea density="compact" variant="outlined" hide-details label="Observaciones" placeholder="ingrese algunos detalles de la orden" 
+                            persistent-placeholder rows="3"/>                        
+                    </v-col>
+                    <v-col cols="12" md="6" sm="6" class="d-flex flex-column justify-end align-end">
+                        <div class="d-flex justify-end align-center">
+                            <small class="mr-2">Sub Total: </small>
+                            <strong>{{ formatedCurrency(data.orden.subTotal, data.fomates.nio) }}</strong>
+                        </div>
+                        <div class="d-flex justify-end align-center">
+                            <small class="mr-2">Iva: </small>
+                            <strong>{{ formatedCurrency(data.orden.iva, data.fomates.nio) }}</strong>
+                        </div>
+                        <div class="d-flex justify-end align-center">
+                            <small class="mr-2">Imptos: </small>
+                            <strong>{{ formatedCurrency(data.orden.imp, data.fomates.nio) }}</strong>
+                        </div>
+                        <div class="d-flex justify-end align-center">
+                            <small class="mr-2">TOTAL: </small>
+                            <strong>{{ formatedCurrency(data.orden.total, data.fomates.nio) }}</strong>
+                        </div>
+                        <div v-if="data.usd" class="d-flex justify-end align-center">
+                            <small class="mr-2">TOTAL $: </small>
+                            <strong>{{ formatedCurrency(data.orden.usdTotal, data.fomates.usd) }}</strong>
+                        </div>
+                    </v-col>
+                </v-row>
+
+            </v-card-text>
+
+            <v-divider/>
+            <v-card-actions>
+                <v-btn color="grey" variant="outlined" @click="closeDialog()">
+                    Cerrar
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+import { formatters } from '@/helpers/formatters';
+import { reactive, ref, watch } from 'vue';
+
+export default {
+    props: {
+        show: {
+            type: Boolean,
+            required: true,
+        },
+        orden: {
+            type: Object,
+            required: false
+        }
+    },
+
+    setup(props) {
+        const localShow = ref(props.show)
+        const localOrden = ref(props.orden)
+        watch(() => props.show, (newValue) => {
+            localShow.value = newValue
+        })
+        watch(() => props.orden, (val) => {
+            localOrden.value = val
+            console.log(val);  
+        })
+
+        const data = reactive({
+            headers: [
+                {title: 'Código', key: 'codigo', align: 'center'},
+                {title: 'Producto', key: 'producto', align: 'center'},
+                {title: 'Cantidad', key: 'codigo', align: 'center'},
+                {title: 'Precio Unit.', key: 'precio', align: 'center'},
+                {title: 'Dscto. %.', key: 'dscto', align: 'center'},
+                {title: 'SubTotal', key: 'subtotal', align: 'center'},
+            ],
+            orden: {
+                numorden: 0,
+                fechaRegistro: 'xx/xx/xxxx',
+                proveedor: '',
+                rucProveedor: '',
+                facturaProv: '',
+                empleado: '',
+                subTotal: 0.00,
+                iva: 0.00,
+                imp: 0.00,
+                total: 0.00,
+                usdTotal: 0.00
+            },
+            nio: true,
+            usd: false,
+            impts: false,
+            fomates: {
+                nio: 'NIO', 
+                usd: 'USD'
+            }
+        })
+
+        return {
+            localShow,
+            localOrden,
+            data
+        }
+    },
+
+    methods: {
+        formatedCurrency(key, currency) {
+            const value = formatters.formatCurrency(key, currency)
+            return value
+        },
+
+        formateDate(dateString) {
+            const value = formatters.formatDate(dateString)
+            return value
+        },
+
+        closeDialog() {
+            this.$emit('closeDialog', false)
+            this.localShow = false
+        }
+    },
+}
+</script>
+
+<style scoped>
+.v-card-item{
+    padding: 8px 12px !important;
+}
+
+#diag-fact{
+    position: relative;
+}
+
+#body-card{
+    z-index: 2;
+}
+
+#checkLabel{
+    font-size: 12px !important;
+}
+.font{
+    font-size: 10px !important;
+}
+</style>
--- src/components/movimientos/Ventas/FacturacionVentas.vue
+++ src/components/movimientos/Ventas/FacturacionVentas.vue
@@ -12,18 +12,34 @@
                 </div>
             </template>
             <template v-slot:append>
-                <v-btn class="bg-red-darken-4 rounded-" @click="openDialog()">
+                <v-btn class="bg-red-darken-4 rounded-" @click="createFactura()">
                     <v-icon>mdi-plus</v-icon>
                     <v-tooltip activator="parent" location="left">Nueva Factura</v-tooltip> 
                 </v-btn>
             </template>
             <v-divider /> 
             <v-row class="pa-2" dense>
-                <v-col cols="6" md="6" sm="6">
+                <v-col cols="6" md="3" sm="3">
+                    <v-text-field color="indigo-darken-4" variant="outlined" append-inner-icon="mdi-calendar" 
+                        density="compact" label="Fecha Desde" v-model="dateDesde" readonly  @click="data.menuDesde = true" 
+                        placeholder="dd/mm/yyyy" persistent-placeholder hide-details/>
+                    <v-dialog v-model="data.menuDesde" width="auto">
+                        <v-date-picker color="indigo-darken-4" v-model="dateDesdeFormatted" />
+                    </v-dialog>
+                </v-col>
+                <v-col cols="6" md="3" sm="3">
+                    <v-text-field color="indigo-darken-4" variant="outlined" append-inner-icon="mdi-calendar" density="compact" 
+                        label="Fecha Hasta" v-model="dateHasta" readonly  @click="data.menuHasta = true" 
+                            placeholder="dd/mm/yyyy" persistent-placeholder hide-details/>
+                    <v-dialog v-model="data.menuHasta" width="auto">
+                        <v-date-picker color="indigo-darken-4" v-model="dateHastaFormatted" />
+                    </v-dialog>
+                </v-col>
+                <v-col cols="12" md="3" sm="3">
                     <v-text-field color="red-darken-4" density="compact" variant="outlined" append-inner-icon="mdi-magnify" label="Buscar productos"
                         hide-details placeholder="Ingrese un texto a buscar..." persistent-placeholder/>
                 </v-col>
-                <v-col cols="6" md="6" sm="6" class="d-flex justify-end align-center">
+                <v-col cols="12" md="3" sm="3" class="d-flex justify-end align-center">
                     <v-btn icon color="red-darken-4" size="small" variant="text" class="mr-2 border">
                         <v-icon>mdi-magnify</v-icon>
                     </v-btn>
@@ -34,9 +50,14 @@
             </v-row>
 
             <v-card-text class="pt-0 px-0">
-                <v-data-table :headers="data.header" :items="data.facturas" class="border" density="compact">
-                    <template v-slot:item.monto="{ item }">
-                        <div>{{ formatedCurrency(item.monto) }}</div>
+                <v-card-subtitle class="d-flex align-center text-center mb-2">
+                    <v-divider /> 
+                    <span class="mx-6 text-grey font-weight-bold">Registros</span>
+                    <v-divider />
+                </v-card-subtitle>
+                <v-data-table :mobile="isMobile" :headers="data.header" :items="data.facturas" class="border" density="compact">
+                    <template v-slot:item.total="{ item }">
+                        <div>{{ formatedCurrency(item.total) }}</div>
                     </template>
                     <template v-slot:item.fecha="{ item }">
                         <div>{{ formatedDate(item.fecha) }}</div>
@@ -56,7 +77,7 @@
 
                         <v-tooltip text="Ver" location="top">
                             <template v-slot:activator="{ props }">
-                                <v-icon v-bind="props" size="small" color="indigo-darken-4">mdi-eye</v-icon>
+                                <v-icon v-bind="props" size="small" color="indigo-darken-4" @click="viewFactura(item)">mdi-eye</v-icon>
                             </template>
                         </v-tooltip>
                     </template>
@@ -67,34 +88,55 @@
                     </template>
                 </v-data-table>
             </v-card-text>
-
-            <v-card-actions>
-
-            </v-card-actions>
         </v-card>
-        <NuevaFactura :show="data.visibleDialog" @closeDialog="closeDialog"/>
+        <NuevaFactura :show="data.editFactura.show" :editar="data.editFactura.editar" :factura="data.editFactura.item" 
+            @closeDialog="closeDialog" :title="data.editFactura.title"/>
+        <ViewVenta :show="data.viewFactura.show" :factura="data.viewFactura.item" @closeDialog="closeDialog"/>
     </div>
 </template>
 
 <script>
 import { formatters } from '@/helpers/formatters.js';
-import { reactive } from 'vue';
+import { reactive, computed, ref, onMounted, onUnmounted } from 'vue';
 import NuevaFactura from './NuevaFactura.vue';
+import ViewVenta from './ViewVenta.vue';
 
 export default {
     components: {
-        NuevaFactura
+        NuevaFactura,
+        ViewVenta
     },
 
     setup() {
+        const screenWidth = ref(window.innerWidth)
+        const isMobile = computed(() => screenWidth.value <= 850)
+        const updateScreen = () => {
+            screenWidth.value = window.innerWidth
+        }
+        onMounted(() => {
+            window.addEventListener('resize', updateScreen)
+        })
+        onUnmounted(() => {
+            window.addEventListener('resize', updateScreen)
+        })
+
+        const dateHastaFormatted = ref(null)
+        const dateDesdeFormatted = ref(null)
+        const dateHasta = computed(() => {
+            return dateHastaFormatted.value ? new Date(dateHastaFormatted.value).toLocaleDateString() : null;
+        })
+
+        const dateDesde = computed(() => {
+            return dateDesdeFormatted.value ? new Date(dateDesdeFormatted.value).toLocaleDateString() : null;
+        })
         const data = reactive({
             header: [
                 {title: '', key: 'opc', align: 'center',},
                 {title: 'Nº Factura', key: 'numFactura', align: 'center'},
                 {title: 'Cliente', key: 'cliente', align: 'center'},
                 {title: 'Vendedor', key: 'vendedor', align: 'center'},
-                {title: 'Fecha', key: 'fecha', align: 'center'},
-                {title: 'Monto', key: 'monto', align: 'center'},
+                {title: 'Fecha Emisión', key: 'fecha', align: 'center'},
+                {title: 'Total', key: 'total', align: 'center'},
                 {title: 'Observaciones', key: 'observaiones', align: 'center'},
                 {title: 'Estado', key: 'estado', align: 'center'},
             ],
@@ -103,36 +145,46 @@ export default {
                 cliente: 'Cliente Prueba',
                 vendedor: 'Vendedor Prueba',
                 fecha: '01/01/2025',
-                monto: 2000,
+                total: 2000,
                 estado: 'Activo'
             }],
-            visibleDialog: false
+            visibleDialog: false,
+
+            editFactura: {
+                show: false,
+                item: {},
+                editar: false,
+                title: ''
+            },
+            viewFactura: {
+                show: false,
+                item: {}
+            },
+            menuDesde: false,
+            menuHasta: false,
         })
 
         return {
-            data
+            data,
+            dateDesde,
+            dateDesdeFormatted,
+            dateHasta,
+            dateHastaFormatted,
+            isMobile
         }
     },
 
     methods: {
-        editFactura(item) {
-            console.log(item);
-        },
-
-        viewFactura(item) {
-            console.log(item);
-        },
-
         formatedCurrency(key) {
             const value = formatters.formatCurrency(key)
             return value
         },
-
+        
         formatedDate(dataString) {
             const value = formatters.formatDate(dataString)
             return value
         },
-
+        
         getStatusColor(status) {
             const statusColors = {
                 'Activo': 'success',
@@ -141,13 +193,29 @@ export default {
             }
             return statusColors[status] || 'grey'
         },
-
-        openDialog() {
-            this.data.visibleDialog = true
+        
+        createFactura() {
+            this.data.editFactura.title = ''
+            this.data.editFactura.show = true
+            this.data.editFactura.editar = false
+            this.data.editFactura.title = 'NUEVA FACTURA'
+        },
+        
+        viewFactura(item) {
+            this.data.viewFactura.show = true
+            this.data.viewFactura.item = item
+        },
+        
+        editFactura(item) {
+            this.data.editFactura.show = true
+            this.data.editFactura.editar = true
+            this.data.editFactura.item = item
+            this.data.editFactura.title = 'EDITAR FACTURA'
         },
 
         closeDialog(val) {  
-            this.data.visibleDialog = val
+            this.data.viewFactura.show = val
+            this.data.editFactura.show = val
         }
 
     }
