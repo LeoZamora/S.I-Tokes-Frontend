6f7df967489756aedd6a389eccd130a46c65624a
LeoZamora <leozamora2003@outlook.com>
2025-06-10 15:28:40 -0600

    Correcion en el detalle de producto

--- src/components/inventario/Productos/modalsProductos/DetallesProducto.vue
+++ src/components/inventario/Productos/modalsProductos/DetallesProducto.vue
@@ -81,7 +81,7 @@
                     <v-col cols="6" sm="4" class="d-flex flex-column align-start py-1 ">
                       <div class="d-flex align-center">
                         <v-icon size="small" class="me-1" color="red-darken-4">mdi-warehouse</v-icon>
-                        <span class="text-caption"><strong>Stock:</strong></span>
+                        <span class="text-caption"><strong>Stock Mínimo:</strong></span>
                       </div>
                       <div class="ms-6 text-caption">{{ producto.cantidadMinima }}</div>
                     </v-col>
