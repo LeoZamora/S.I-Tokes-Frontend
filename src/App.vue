f68ff82a47e72ab57987e699f1813a8e24d3b3bd
rolan <rmeidna.t@gmail.com>
2025-07-16 23:07:37 -0600

    ADAPTACIONES

--- src/App.vue
+++ src/App.vue
@@ -5,54 +5,72 @@
       <AppBar @toggle-drawer="toggleDrawer" @nameRoute="nameTab" @logout="clearApp"/>
       <v-navigation-drawer id="drawer" v-model="data.drawer" class="position-fixed">
         <v-list density="compact" v-model:selected="data.selectedItems" :mandatory="true" select-strategy="leaf">
-            <v-list-subheader><small class="font-weight-bold">DASHBOARD</small></v-list-subheader>
-            <v-list-item prepend-icon="mdi-home" color="primary" title="Inicio" :lines="true" rounded class="mx-2" value="Inicio" @click="clearApp()"/>
-            <v-list-subheader><small class="font-weight-bold">MOVIMIENTOS</small></v-list-subheader>
-            <v-list-group prepend-icon="mdi-cash-register">
-                <template v-slot:activator="{ props }">
-                    <v-list-item v-bind="props" rounded value="Venta" :lines="true" color="primary" class="mx-2" title="Ventas"/>
-                </template>
-                <v-list-item class="mx-2" rounded :lines="true" color="primary" v-for="([title, route, icon], i) in ventasFiltradas" 
-                  :key="i" :value="title" @click="nameTab(route)">
-                    <small>- {{ title }}</small>
-                </v-list-item>
-            </v-list-group>
-            <v-list-group prepend-icon="mdi-cart-arrow-down">
-                <template v-slot:activator="{ props }">
-                    <v-list-item v-bind="props" rounded value="Compras" :lines="true" color="primary" class="mx-2" title="Compras"/>
-                </template>
-                <v-list-item class="mx-2" rounded :lines="true" color="primary" v-for="([title, route, icon], i) in comprasFiltradas"
-                    :key="i" :value="title" @click="nameTab(route)">
-                    <small>- {{ title }}</small>
-                </v-list-item>
-            </v-list-group>
-            <v-list-subheader><small class="font-weight-bold">GESTIÓN GENERAL</small></v-list-subheader>
-            <v-list-group prepend-icon="mdi-package-variant">
-                <template v-slot:activator="{ props }">
-                    <v-list-item v-bind="props" rounded value="Inventario" :lines="true" color="primary" class="mx-2" title="Inventario"/>
-                </template>
-                <v-list-item class="mx-2" rounded :lines="true" color="primary" v-for="([title, route, icon], i) in data.managerStock" 
-                    :key="i" :value="title" @click="nameTab(route)">
-                    <small>- {{ title }}</small>
-                </v-list-item>
-            </v-list-group>
+          <v-list-subheader><small class="font-weight-bold">DASHBOARD</small></v-list-subheader>
+          <v-list-item prepend-icon="mdi-home" color="primary" title="Inicio" :lines="true" rounded class="mx-2"
+                       value="Inicio" @click="clearApp()"/>
+          <v-list-subheader><small class="font-weight-bold">MOVIMIENTOS</small></v-list-subheader>
+          <v-list-group prepend-icon="mdi-cash-register">
+            <template v-slot:activator="{ props }">
+              <v-list-item v-bind="props" rounded value="Venta" :lines="true" color="primary" class="mx-2"
+                           title="Ventas"/>
+            </template>
+            <v-list-item class="mx-2" rounded :lines="true" color="primary"
+                         v-for="([title, route, icon], i) in ventasFiltradas"
+                         :key="i" :value="title" @click="nameTab(route)">
+              <small>- {{ title }}</small>
+            </v-list-item>
+          </v-list-group>
+          <v-list-group prepend-icon="mdi-cart-arrow-down">
+            <template v-slot:activator="{ props }">
+              <v-list-item v-bind="props" rounded value="Compras" :lines="true" color="primary" class="mx-2"
+                           title="Compras"/>
+            </template>
+            <v-list-item class="mx-2" rounded :lines="true" color="primary"
+                         v-for="([title, route, icon], i) in comprasFiltradas"
+                         :key="i" :value="title" @click="nameTab(route)">
+              <small>- {{ title }}</small>
+            </v-list-item>
+          </v-list-group>
+          <v-list-subheader><small class="font-weight-bold">GESTIÓN GENERAL</small></v-list-subheader>
+          <v-list-group prepend-icon="mdi-package-variant">
+            <template v-slot:activator="{ props }">
+              <v-list-item v-bind="props" rounded value="Inventario" :lines="true" color="primary" class="mx-2"
+                           title="Inventario"/>
+            </template>
+            <v-list-item class="mx-2" rounded :lines="true" color="primary"
+                         v-for="([title, route, icon], i) in data.managerStock"
+                         :key="i" :value="title" @click="nameTab(route)">
+              <small>- {{ title }}</small>
+            </v-list-item>
+          </v-list-group>
           <v-list-group prepend-icon="mdi-account-group">
             <template v-slot:activator="{ props }">
-              <v-list-item v-bind="props" rounded value="Inventario" :lines="true" color="primary" class="mx-2" title="Clientes"/>
+              <v-list-item v-bind="props" rounded value="Inventario" :lines="true" color="primary" class="mx-2"
+                           title="Clientes"/>
             </template>
-            <v-list-item class="mx-2" rounded :lines="true" color="primary" v-for="([title, icon, route], i) in data.clientes"
-              :key="i" :value="title" @click="nameTab(title)">
+            <v-list-item class="mx-2" rounded :lines="true" color="primary"
+                         v-for="([title, icon, route], i) in data.clientes"
+                         :key="i" :value="title" @click="nameTab(title)">
               <small>- {{ title }}</small>
             </v-list-item>
           </v-list-group>
         </v-list>
         <template v-slot:append>
-            <v-divider class="mx-4" />
-            <div class="d-flex flex-column align-center pa-2">
-                <h6 class="mt-2">
-                  DevoDigital - copyright @{{ new Date().getFullYear() }}
-                </h6>
+          <v-divider class="mx-4"/>
+          <div class="d-flex justify-center pa-2">
+            <div>
+              <v-img width="50" src="/32px.png"></v-img>
             </div>
+            <v-divider vertical class="mx-3"></v-divider>
+            <div class="d-flex flex-column align-center">
+              <h6>
+                Inversiones Zafiro
+              </h6>
+              <h6>
+                v1.0.0
+              </h6>
+            </div>
+          </div>
         </template>
       </v-navigation-drawer>
       <v-main class="w-100 position-relative">
@@ -65,16 +83,45 @@
           </transition>
         </router-view>
       </v-main>
+      <v-footer
+          app
+          inset
+          style="background-color: #FAFAFA"
+          absolute
+          height="45"
+          class="px-0"
+      >
+        <v-row dense class="pa-1">
+          <v-col>
+            <div></div>
+          </v-col>
+          <v-col class="d-flex flex-column align-center">
+            <span
+                style="color: black; font-size: 9px;"
+            >
+            &copy;DevoDigital - {{ new Date().getFullYear() }}
+          </span>
+            <span
+                style="color: grey; font-size: 8px"
+            >
+            Todos los derechos reservados.
+          </span>
+          </v-col>
+          <v-col class="pa-0 pr-2 justify-end">
+
+          </v-col>
+        </v-row>
+      </v-footer>
     </v-app>
   </v-layout>
 </template>
 <script>
-import { ref, reactive, computed, onMounted, onUnmounted } from 'vue';
+import {ref, reactive, computed, onMounted, onUnmounted} from 'vue';
 import AppBar from './components/layout/AppBar.vue'
 import TabsRoutes from './components/widgets/TabsRoutes.vue';
 import LoginAuth from './components/login/LoginAuth.vue';
 import environment from './helpers/environment.js';
-import { useStore } from './store';
+import {useStore} from './store';
 
 export default {
   mounted() {
@@ -128,8 +175,8 @@ export default {
         ['Usuarios'],
       ],
       views: {
-        fact: false, 
-        orden: false, 
+        fact: false,
+        orden: false,
         roles: false,
         user: false
       },
@@ -176,16 +223,18 @@ export default {
       const token = this.store.getInfoUser()
       const ventanas = token.ventanasAcceso.split(",")
       this.data.views = {
-        fact: true, 
-        orden: true, 
+        fact: true,
+        orden: true,
       }
-      
+
       ventanas.map(item => {
-        switch(item) {
-          case '3': this.data.views.fact = false
-              break;
-          case '7': this.data.views.orden = false
-              break;
+        switch (item) {
+          case '3':
+            this.data.views.fact = false
+            break;
+          case '7':
+            this.data.views.orden = false
+            break;
         }
       })
     },
@@ -210,11 +259,11 @@ export default {
         this.data.selectedItems.push(name)
       }
 
-      this.$router.push({ name: name })
+      this.$router.push({name: name})
     },
 
     clearApp() {
-      this.$router.push({ path: '/' })
+      this.$router.push({path: '/'})
       this.data.nameTabs = []
       this.data.nameCurrentTab = ''
       this.data.activeTab = null
@@ -233,7 +282,7 @@ export default {
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
-#app{
+#app {
   font-family: Poppins;
 }
 
@@ -243,9 +292,11 @@ export default {
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
