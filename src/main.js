8967e741539e2ff83cd1bcc82d3ca7173a356b1e
RolandoMedinaMac <Rmeidna.T@gmail.com>
2026-07-30 10:26:32 -0600

    feat: bodegas listo

--- src/main.js
+++ src/main.js
@@ -13,8 +13,8 @@ import 'vuetify/styles'
 import Axios from 'axios'
 import 'intro.js/minified/introjs.min.css';
 
-// Axios.defaults.baseURL = 'http://localhost:5091/'
-Axios.defaults.baseURL = 'https://inversiones-zafiro.com/devodigital/'
+ Axios.defaults.baseURL = 'http://localhost:5091/'
+//Axios.defaults.baseURL = 'https://inversiones-zafiro.com/devodigital/'
 
 const pinia = createPinia();
 const app = createApp(App);
